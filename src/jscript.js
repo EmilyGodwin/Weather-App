e0a5c7cda1eccb0668a1bd2d311780cc05cb9753
EmilyGodwin <godwie20@gmail.com>
2022-04-16 11:43:29 -0500

    Added search function
    Added a search function and fixed css.

--- src/jscript.js
+++ src/jscript.js
@@ -47,7 +47,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "12e817575070bcd60eb64f87187b9c19";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "12e817575070bcd60eb64f87187b9c19";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
